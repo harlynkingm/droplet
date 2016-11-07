1fc90f7ef6367e17048e45f73ec7c4424938976b
Max Harlynking <mharlynk@andrew.cmu.edu>
2016-11-06 19:23:18 -0500

    Initial Commit

---
+++ Droplight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Droplight
+//
+//  Created by MHK on 11/6/16.
+//  Copyright © 2016 MHK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
